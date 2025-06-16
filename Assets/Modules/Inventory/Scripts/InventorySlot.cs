2a5f52108d2858df43cb979bfe8a6e141d0d80dc
TobyMunyard <tobymunyard@gmail.com>
2025-06-16 14:31:57 +1200

    Completed InventorySlot Logic
    * Completed slot wrapper for InventoryItem objects.
    * Added logic for adding items to slots and checking if the slot if empty.

--- Assets/Modules/Inventory/Scripts/InventorySlot.cs
+++ Assets/Modules/Inventory/Scripts/InventorySlot.cs
@@ -1,6 +1,54 @@
 using UnityEngine;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot
 {
-   
+    public InventoryItem item;
+
+    public int quantity;
+
+    public bool isSlotEmpty()
+    {
+        if (item == null || quantity == 0) return true;
+
+        return false;
+    }
+
+    public bool addItem(InventoryItem item)
+    {
+        if (isSlotEmpty())
+        {
+            this.item = item;
+            quantity++;
+            return true;
+        }
+        else if (this.item.itemID == item.itemID && (quantity + 1 <= item.maxStackSize))
+        {
+            this.item = item;
+            quantity++;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool addItem(InventoryItem item, int quantity)
+    {
+        if (isSlotEmpty())
+        {
+            this.item = item;
+            this.quantity = Mathf.Min(quantity, item.maxStackSize);
+            return true;
+        }
+
+        if (this.item.itemID == item.itemID && this.quantity < item.maxStackSize)
+        {
+            int spaceLeft = item.maxStackSize - this.quantity;
+            this.quantity += Mathf.Min(spaceLeft, quantity);
+            return true;
+        }
+
+        return false;
+    }
 }
